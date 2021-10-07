12b4fc9315aaaff8bda0faf47017b29810a9864f
Jon K <Johnakao@gmail.com>
2021-10-07 13:13:14 -0700

    grid traveler using tabulation o(mn)

--- gridTraveler.js
+++ gridTraveler.js
@@ -1,7 +1,17 @@
 // o(mn) TC o(mn) SC
 const gridTraveler = (m, n) => {
-  const table = Array(m + 1).fill().map(() => Array(n+1))
-  console.log(table)
+  const table = Array(m+1)
+  .fill()
+  .map(() => Array(n+1).fill(0))
+  table[1][1] = 1
+  for (let row = 0; row <= m; row++) {
+    for (let col = 0; col <= n; col++) {
+      let curr = table[row][col]
+      if (row + 1 <= m) table[row + 1][col] += curr
+      if (col + 1 <= n) table[row][col + 1] += curr
+    }
+  }
+  return table[m][n]
 }
-// gridTraveler(1,1) = 1
+
 console.log(gridTraveler(3,3)) //6
